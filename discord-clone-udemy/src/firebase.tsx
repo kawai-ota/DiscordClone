dc45246aaa913c91895e3ddeab34ef7efd4cb54d
河合桜汰 <o.k.20020929@icloud.com>
2024-01-21 01:04:24 +0900

    Firebaseの導入と初期をした

---
+++ discord-clone-udemy/src/firebase.tsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7vaobQVH-ibWH9q9B7FJKywW3zGCCfM0",
+  authDomain: "discord-clone-udemy-8141e.firebaseapp.com",
+  projectId: "discord-clone-udemy-8141e",
+  storageBucket: "discord-clone-udemy-8141e.appspot.com",
+  messagingSenderId: "1018396456554",
+  appId: "1:1018396456554:web:ce6465e285f219bd05bc13",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
