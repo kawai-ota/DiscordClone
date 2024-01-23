6053ebef2920310e5c8f67c355d9d2777ea50073
河合桜汰 <o.k.20020929@icloud.com>
2024-01-24 01:22:00 +0900

    型定義などを行った

--- discord-clone-udemy/src/features/userSlice.ts
+++ discord-clone-udemy/src/features/userSlice.ts
@@ -1,8 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { InitialUserState } from "../Types";
+
+const initialState: InitialUserState = {
+  user: null,
+};
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: { user: null },
+  initialState,
   reducers: {
     login: (state, action) => {
       state.user = action.payload;
