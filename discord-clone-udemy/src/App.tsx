6c0ce975b626417a7a5b818f5eb6b1827efc0f2c
河合桜汰 <o.k.20020929@icloud.com>
2024-01-24 02:15:28 +0900

    useSelectorなどの型指定をした。Reduxは型指定がないのでこのような処理をして正確な型を返せるようにした

--- discord-clone-udemy/src/App.tsx
+++ discord-clone-udemy/src/App.tsx
@@ -4,9 +4,10 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Chat from "./components/chat/Chat";
 import { useSelector } from "react-redux";
 import Login from "./components/login/Login";
+import { useAppSelector } from "./app/hooks";
 
 function App() {
-  const user = null;
+  const user = useAppSelector((state) => state.user);
 
   return (
     <div className="App">
--- discord-clone-udemy/src/app/store.ts
+++ discord-clone-udemy/src/app/store.ts
@@ -4,3 +4,7 @@ import userReducer from "../features/userSlice";
 export const store = configureStore({
   reducer: userReducer,
 });
+
+export type AppDispatch = typeof store.dispatch;
+
+export type RootState = ReturnType<typeof store.getState>;
