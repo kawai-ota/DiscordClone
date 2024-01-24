3e5aac5d4918025ba3e4c71f092514fd88654581
河合桜汰 <o.k.20020929@icloud.com>
2024-01-25 01:42:33 +0900

    ログインボタンをクリックしたらsignIn関数を発火させてsignInWithPopup関数を実行させるようにした

--- discord-clone-udemy/src/app/hooks.ts
+++ discord-clone-udemy/src/app/hooks.ts
@@ -3,4 +3,5 @@ import { AppDispatch, RootState } from "./store";
 import { useSelector } from "react-redux";
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
+//typeof store.dispatchとすることによって、dispatchの型を取り出している。それを型ヘルパーするとuseDispatchが使用できるようになる。
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- discord-clone-udemy/src/components/login/Login.tsx
+++ discord-clone-udemy/src/components/login/Login.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import "./Login.scss";
 import { Button } from "@mui/material";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../firebase";
 
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message);
+    });
+  };
+
   return (
     <div className="login">
       <div className="loginLogo">
         <img src="./discordIcon.png" alt="" />
       </div>
-      <Button className="button">ログイン</Button>
+      <Button onClick={signIn} className="button">
+        ログイン
+      </Button>
     </div>
   );
 };
